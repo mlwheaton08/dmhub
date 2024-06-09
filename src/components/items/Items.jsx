107e864df6c4c304a80c67f622d521e0093f22ff
Mason Wheaton <mlwheaton06@gmail.com>
2024-06-09 05:24:53 -0500

    directory shift

--- src/components/items/Items.jsx
+++
@@ -1,8 +0,0 @@
-export function Items() {
-
-    return (
-        <div className="mt-nav-height">
-            <h1>Items Page</h1>
-        </div>
-    )
-}
--- src/components/recipes/Recipes.jsx
+++
@@ -1,8 +0,0 @@
-export function Recipes() {
-
-    return (
-        <div className="mt-nav-height">
-            <h1>Recipes Page</h1>
-        </div>
-    )
-}
